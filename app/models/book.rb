48eab7c43f85cbad7cf24ca8fc713045a0687529
Cao Quang Binh <binhcq@asiantech.vn>
2015-12-01 08:37:55 +0700

    Add fail rspec test

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,8 @@ class Book < ActiveRecord::Base
   # def error_method
   #   foo = 1
   # end
+
+  def rspec_fail
+    foo = "bar"
+  end
 end
--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe BooksController, type: :controller do
+  describe "GET #index" do
+    it "responds successfully with an HTTP 300 status code" do
+      get :index
+      expect(response).to be_success
+      expect(response).to have_http_status(300)
+    end
+  end
 end
