9a0566983a4678261a722a97a92fb5d6b115345d
Cao Quang Binh <binhcq@asiantech.vn>
2015-12-01 09:11:06 +0700

    fix rubocop

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe BooksController, type: :controller do
-  describe "GET #index" do
-    it "responds successfully with an HTTP 300 status code" do
+  describe 'GET #index' do
+    it 'this test case will fail' do
       get :index
       expect(response).to be_success
       expect(response).to have_http_status(300)
